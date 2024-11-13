11ff7285aab9c3a094bf7fa902ad5ad1851186af
stivenferreira99 <stivendanielferreiraflorentin@gmail.com>
2024-11-12 21:38:17 -0300

    a

---
+++ datos tablas/alumno.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select * from alumno
+LIMIT 0, 1000
+
+-- Date: 2024-11-12 21:37
+*/
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (1,'María','González','2020100040','2001-07-15','34567891',3);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (2,'Roger','Garay','2017100330','2002-04-22','23456789',2);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (3,'María','González','2021100157','1991-06-25','34567891',2);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (4,'Valeria','Hernández','202010004','1999-01-01','45678901',1);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (5,'Felipe','Lopez','202010005','1998-06-18','56789012',1);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (6,'María','Pérez','202010006','1997-12-12','67890123',2);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (7,'Pedro','Ruiz','202010007','2003-05-05','78901234',3);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (8,'Ana','Mendoza','202010008','1996-08-08','89012345',1);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (9,'Jorge','Rivas','202010009','2000-07-07','90123456',1);
+INSERT INTO `` (`id_alumno`,`nombre`,`apellido`,`matricula`,`fecha_nacimiento`,`ci`,`id_carrera`) VALUES (10,'Sofia','Cruz','202010010','1995-11-11','01234567',2);
